24c15452dac8b68c7b39cb97c878184ff01c9158
li <502550582@qq.com>
2020-01-19 00:27:34 -0800

    second change

--- code.c
+++ code.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 int main(){
 	int a=100;
+	int b=0;
 	for(;a>0;a--){
-		if(a%10==0){
-			printf("this my test!\n");
+		if(a%10 == 0){
+			b++;
+			printf("\nthis my %d group!\n",b);
+		}
+		if(a%5 == 0){
+			printf("this number is %d\t",a-1);
 		}
 	}
+	printf("\n");
 }
